581d8ac47c777030148288ed40492451e7c4a288
sumit <sgupta40@sapient.com>
2016-08-05 17:55:33 +0530

    updated the files

--- src/test/java/hello/GreetingControllerTests.java
+++ src/test/java/hello/GreetingControllerTests.java
@@ -32,6 +32,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import controllers.Application;
+
 /**
  * @author Greg Turnquist
  */
