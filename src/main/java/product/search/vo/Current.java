9685ea66218d21c06b26529de4aab0d48b251cc1
gaurav <gsood@sapient.com>
2016-08-06 12:07:36 +0530

    Product Search API

---
+++ src/main/java/product/search/vo/Current.java
@@ -0,0 +1,19 @@
+package product.search.vo;
+
+public class Current {
+	private String desc;
+	private String value;
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public String getValue() {
+		return value;
+	}
+	public void setValue(String value) {
+		this.value = value;
+	}
+	
+}
---
+++ src/main/java/product/search/vo/ProductData.java
@@ -0,0 +1,57 @@
+package product.search.vo;
+
+public class ProductData {
+
+	private String title;
+	private String price;
+	private String productPartNumber;
+	private String altText;
+	private String productMainImage;
+	private String productDescription;
+	private String ratings;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getProductPartNumber() {
+		return productPartNumber;
+	}
+	public void setProductPartNumber(String productPartNumber) {
+		this.productPartNumber = productPartNumber;
+	}
+	public String getAltText() {
+		return altText;
+	}
+	public void setAltText(String altText) {
+		this.altText = altText;
+	}
+	public String getProductMainImage() {
+		return productMainImage;
+	}
+	public void setProductMainImage(String productMainImage) {
+		this.productMainImage = productMainImage;
+	}
+	public String getProductDescription() {
+		return productDescription;
+	}
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+	public String getRatings() {
+		return ratings;
+	}
+	public void setRatings(String ratings) {
+		this.ratings = ratings;
+	}
+	
+	
+	
+}
