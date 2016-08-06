380401e5791fb9d7236aa9af494692c6583099cc
gaurav <gsood@sapient.com>
2016-08-06 13:23:55 +0530

    CORS issue

--- src/main/java/controllers/Application.java
+++ src/main/java/controllers/Application.java
@@ -2,10 +2,25 @@ package controllers;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class Application {
 
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                //registry.addMapping("/product").allowedOrigins("http://localhost:8100");
+            	registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+	
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
